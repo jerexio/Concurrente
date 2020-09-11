57fa49340fe682115369e21a6721ca6834f82a13
Jeremias <jeremiasrodriguez05@gmail.com>
2020-09-11 12:00:34 -0300

    Llego la hora de imprimir latras

---
+++ EJ_3.Letras/Impresion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3.EJ_3.Letras;
+
+/**
+ *
+ * @author jerem
+ */
+public class Impresion extends Thread {
+
+    private Letra letra;
+
+    public Impresion(Letra letra) {
+        this.letra = letra;
+    }
+
+    public void run() {
+        System.out.println("\nLa letra " + letra.getLetra() + " esta lista");
+        
+        synchronized (letra) {
+            int cant = (int) letra.getLetra();
+
+            for (int i = 64; i < cant; i++) {
+                System.out.print(letra.getLetra());
+            }
+        }
+    }
+}
