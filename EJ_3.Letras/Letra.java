c5f1c154723bbc2db5bc4050d8da29aec6d54d8a
Jeremias <jeremiasrodriguez05@gmail.com>
2020-09-11 11:59:25 -0300

    La clase Letra aparece

---
+++ EJ_3.Letras/Letra.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3.EJ_3.Letras;
+
+/**
+ *
+ * @author jerem
+ */
+public class Letra {
+    private char letra;
+
+    public Letra(char letra) {
+        this.letra = letra;
+    }
+    
+    public char getLetra(){
+        return this.letra;
+    }
+}
