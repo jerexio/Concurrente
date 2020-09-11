be8e23305bb8999457133c29c64e059bd8a7361d
Jeremias <jeremiasrodriguez05@gmail.com>
2020-09-11 12:08:27 -0300

    Cliente pa comprar

---
+++ EJ_6.Cajeras/Cliente.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2.EJ_6.Cajera_B;
+
+/**
+ *
+ * @author jerem
+ */
+public class Cliente {
+    private String nombre;
+    private int[] carroCompra;
+
+    public Cliente(String nombre, int[] carroCompra) {
+        this.nombre = nombre;
+        this.carroCompra = carroCompra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int[] getCarroCompra() {
+        return carroCompra;
+    }
+
+    public void setCarroCompra(int[] carroCompra) {
+        this.carroCompra = carroCompra;
+    }
+    
+    
+}
