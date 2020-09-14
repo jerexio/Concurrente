bf88ad53aa9c5cc7a20084f21e4fc5b761a10bd9
Jeremias <jeremiasrodriguez05@gmail.com>
2020-09-14 10:44:41 -0300

    New Main

--- EJ_3.Letras/Main.java
+++ EJ_3.Letras/Main.java
@@ -10,15 +10,16 @@ package TP3.EJ_3.Letras;
  * @author jerem
  */
 public class Main {
-    public static void main(String[] args){
-        Letra[] letras = new Letra[3];
+
+    public static void main(String[] args) {
+        
         Impresion[] imp = new Impresion[3];
+        SelectorTurno turno = new SelectorTurno();
         
-        for(int i = 0; i <= 2; ++i){
-            imp[i] = new Impresion(new Letra((char)(i + 65)));
-            
+        for (int i = 0; i <= 2; ++i) {
+            imp[i] = new Impresion(new Letra((char) (i + 65), i + 1), turno);
         }
-        
+
         imp[0].start();
         imp[1].start();
         imp[2].start();
